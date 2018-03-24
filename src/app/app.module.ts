b48e5a02a5254b7220785ae6801bbeef82e68fe1
unknown <rajarohit.in@gmail.com>
2018-03-24 16:28:46 +0530

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './common/header/header.component';
+import { NavebarComponent } from './common/navebar/navebar.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { ToasterComponent } from './common/toaster/toaster.component';
+import { SpinnerComponent } from './common/spinner/spinner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    NavebarComponent,
+    FooterComponent,
+    ToasterComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
